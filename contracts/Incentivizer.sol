efe02ce4261e5f2cd92826a09175523316d30cd2
IntegralTeam O <repo@sfxdx.ru>
2021-03-02 17:51:01 +0300

    adding new version of prototype

--- contracts/Incentivizer.sol
+++ contracts/Incentivizer.sol
@@ -1,166 +1,230 @@
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/math/Math.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
-import "@openzeppelin/contracts/utils/EnumerableSet.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/GSN/Context.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/proxy/Initializable.sol";
+
 import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
-import "@uniswap/v2-periphery/contracts/libraries/UniswapV2Library.sol";
-import "./templates/Initializable.sol";
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
 
+import "./libraries/RewardsDistributionRecipient.sol";
+import "./interfaces/IStakingRewards.sol";
+import "./libraries/UniswapV2Library.sol";
 
-/**
- * @title Incentivizer
- * @dev This contract allows users to incentivize their LP profits in XBE.
- */
-contract Incentivizer is Initializable {
+contract Incentivizer is IStakingRewards, RewardsDistributionRecipient, ReentrancyGuard, Initializable, Context {
 
     using SafeMath for uint256;
-    using Address for address;
-    using EnumerableSet for EnumerableSet.AddressSet;
     using SafeERC20 for IERC20;
 
-    mapping(address => mapping(address => uint256)) public sharesBalances;
+    /* ========== STATE VARIABLES ========== */
 
-    address public scheduler;
-    address public rewardDistribution;
+    IERC20 public baseToken;
+    IERC20 public rewardsToken;
+    IERC20 public stakingToken;
 
-    IERC20 public providerToken; // eurxb token
-    IERC20 public incentivizeToken; // XBE token
-    IERC20 public providerIncentivizeTokensPairAddress; // pair XBE-eurxb
+    IUniswapV2Pair public pairAddress; // pair XBE-eurxb
+
+    uint256 public globalRewardsDuration;
+    uint256 public buyOnScheduleRate;
+    uint256 public globalPeriodFinish;
+    uint256 public lastBuyOnScheduleTime;
+
+    uint256 public rewardRate;
+    uint256 public periodFinish;
+    uint256 public rewardsDuration;
+    uint256 public lastUpdateTime;
+
+    uint256 public rewardPerTokenStored;
+
+    mapping(address => uint256) public userRewardPerTokenPaid;
+    mapping(address => uint256) public rewards;
+
+    // mapping(address => uint256) public lastTimeBalanceChanged;
+
+    uint256 private _totalSupply;
+    mapping(address => uint256) private _balances;
+
     IUniswapV2Router02 public uniswapRouter;
 
-//    EnumerableSet.UintSet public startTimes;
-//    EnumerableSet.UintSet public endTimes;
-//    EnumerableSet.UintSet public timesToAmounts;
+    /* ========== EVENTS ========== */
 
-    event Staked(address indexed user, address indexed stakingToken, uint256 amount);
-    event Withdrawn(address indexed user, address indexed stakingToken, uint256 amount);
-    event RewardPaid(address indexed user, uint256 reward);
     event RewardAdded(uint256 reward);
+    event Staked(address indexed user, uint256 amount);
+    event Withdrawn(address indexed user, uint256 amount);
+    event RewardPaid(address indexed user, uint256 reward);
 
-    modifier onlyScheduler {
-        require(_msgSender() == scheduler, "!scheduler");
+    /* ========== MODIFIERS ========== */
+
+    modifier updateReward(address account) {
+        rewardPerTokenStored = rewardPerToken();
+        lastUpdateTime = lastTimeRewardApplicable();
+        if (account != address(0)) {
+            // lastTimeBalanceChanged[msg.sender] = block.timestamp;
+            rewards[account] = earned(account);
+            userRewardPerTokenPaid[account] = rewardPerTokenStored;
+        }
         _;
     }
 
-    modifier onlyRewardDistribution {
-        require(_msgSender() == rewardDistribution, "Caller is not reward distribution.");
-        _;
-    }
+    /* ========== INITIALIZER ========== */
 
     function configure(
-        address _scheduler,
-        address _rewardDistribution,
+        address _rewardsDistribution,
         address _uniswapRouter,
         address _eurxbAddress,
-        address _xbeAddress
-//        uint256[] calldata _startTimes,
-//        uint256[] calldata _endTimes,
-//        uint256[] calldata _timesToAmounts
-    ) external initializer {
-//        require(_startTimes.length == _endTimes.length, "!startEnd");
-//        require(_startTimes.length == _timesToAmounts.length, "!startTimes");
-//        require(_endTimes.length == _timesToAmounts.length, "!endTimes");
-        scheduler = _scheduler;
-        rewardDistribution = _rewardDistribution;
-        uniswapRouter = IUniswapV2Router02(_uniswapRouter); // 0x7a250d5630b4cf539739df2c5dacb4c659f2488d
-        providerIncentivizeTokensPairAddress = UniswapV2Library.pairFor(uniswapRouter.factory(), _eurxbAddress, _xbeAddress);
-        providerToken = IERC20(_eurxbAddress);
-        incentivizeToken = IERC20(_xbeAddress);
-//        for (uint256 i = 0; i < _startTimes.length; i++) {
-//            startTimes.add(_startTimes[i]);
-//            endTimes.add(_endTimes[i]);
-//            timesToAmounts.add(_timesToAmounts[i]);
-//        }
-    }
-
-    function budget() view external returns(uint256) {
-        return providerToken.balanceOf(address(this));
-    }
-
-    function buyOnSchedule(address _lpTokenAddress) external onlyScheduler {
-//        uint256 _providerTokenAmountToSpend = getAmountFromTime(block.timestamp);
-//        require(_providerTokenAmountToSpend > 0, "!scheduled");
-//        uint256 _boughtIncentivizeToken = 0; //TODO: buy from pair
-//        incentivize(_lpTokenAddress, _boughtIncentivizeToken);
-    }
-
-    function notifyRewardAmount(uint256 _reward)
-        external
-        onlyRewardDistribution
-    {
-        providerToken.safeTransferFrom(_msgSender(), address(this), _reward);
-//        if (block.timestamp >= periodFinish) {
-//            rewardRate = _reward.div(DURATION);
-//        } else {
-//            uint256 remaining = periodFinish.sub(block.timestamp);
-//            uint256 leftover = remaining.mul(rewardRate);
-//            rewardRate = _reward.add(leftover).div(DURATION);
-//        }
-//        lastUpdateTime = block.timestamp;
-//        periodFinish = block.timestamp.add(DURATION);
-        emit RewardAdded(_reward);
-    }
-
-    function stake(address _lpTokenAddress, uint256 _amount) external {
-        require(_amount > 0, "Cannot stake 0");
-        address user = _msgSender();
-        IERC20(_lpTokenAddress).safeTransferFrom(user, address(this), _amount);
-        sharesBalances[_lpTokenAddress][user] = sharesBalances[user][_msgSender()].add(_amount);
-        emit Staked(user, _lpTokenAddress, _amount);
-    }
-
-    function withdraw(address _lpTokenAddress, uint256 _amount) external {
-        require(_amount > 0, "Cannot withdraw 0");
-        address user = _msgSender();
-        IERC20(_lpTokenAddress).safeTransfer(user, _amount);
-        sharesBalances[_lpTokenAddress][user] = sharesBalances[_lpTokenAddress][user].sub(_amount);
-        emit Withdrawn(user, _lpTokenAddress, _amount);
-    }
-
-//    function addSchedule(
-//        uint256 _startTime,
-//        uint256 _endTime,
-//        uint256 _amount
-//    ) onlyScheduler external {
-//        startTimes.add(_startTime);
-//        endTimes.add(_endTime);
-//        timesToAmounts.add(_amount);
-//    }
-//
-//    function removeSchedule(
-//        uint256 _startTime,
-//        uint256 _endTime,
-//        uint256 _amount
-//    ) onlyScheduler external {
-//        startTimes.remove(_startTime);
-//        endTimes.remove(_endTime);
-//        timesToAmounts.remove(_amount);
-//    }
-//
-//    function getAmountFromTime(uint256 _timestamp) internal view returns(uint256) {
-//        for (uint256 i = 0; i < startTimes.length(); i++) {
-//          if (startTimes.at(i) < _timestamp && endTimes.at(i) > _timestamp) {
-//              return timesToAmounts.at(i);
-//          }
-//        }
-//        return 0;
-//    }
-
-    function incentivize(
+        address _xbeAddress,
         address _lpTokenAddress,
-        uint256 _incentivizeTokenAmount
-    ) internal {
-//        for (uint256 i = 0; i < sharesHolders[_lpTokenAddress].length(); i++) {
-//            address _shareHolder = sharesHolders[_lpTokenAddress].at(i);
-//            uint256 _holderBalance = sharesBalances[_lpTokenAddress][_shareHolder];
-//            if (_holderBalance > 0) {
-//                // TODO: use shareBalance as rank in distribution
-//                uint256 _amountIncetivizeTokens = 0;
-//                IERC20(incentivizeToken).transfer(_shareHolder, _amountIncetivizeTokens);
-//            }
-//        }
+        uint256 _rewardsDuration, // 1 days
+        uint256 _globalRewardsDuration // 25 days
+    ) external initializer {
+        rewardsDistribution = _rewardsDistribution;
+        uniswapRouter = IUniswapV2Router02(_uniswapRouter); // 0x7a250d5630b4cf539739df2c5dacb4c659f2488d
+        pairAddress = IUniswapV2Pair(UniswapV2Library.pairFor(uniswapRouter.factory(), _eurxbAddress, _xbeAddress));
+        stakingToken = IERC20(_lpTokenAddress);
+        rewardsToken = IERC20(_xbeAddress);
+        baseToken = IERC20(_eurxbAddress);
+        globalRewardsDuration = _globalRewardsDuration;
+        rewardsDuration = _rewardsDuration;
+    }
+
+    /* ========== VIEWS ========== */
+
+    function totalSupply() external view override returns(uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) external view override returns(uint256) {
+        return _balances[account];
+    }
+
+    function chooseMinTime(uint256 otherTime) internal view returns(uint256) {
+        return Math.min(block.timestamp, otherTime);
+    }
+
+    function lastTimeRewardApplicable() public view override returns(uint256) {
+        return chooseMinTime(periodFinish);
+    }
+
+    function rewardPerToken() public view override returns(uint256) {
+        if (_totalSupply == 0) {
+            return rewardPerTokenStored;
+        }
+        return rewardPerTokenStored.add(
+            lastTimeRewardApplicable().sub(lastUpdateTime).mul(rewardRate).mul(1e18).div(_totalSupply)
+        );
+    }
+
+    function earned(address account) public view override returns(uint256) {
+        // uint256 lastTimeStaked = lastTimeBalanceChanged[account];
+        return _balances[account].mul(rewardPerToken().sub(userRewardPerTokenPaid[account])).div(1e18).add(rewards[account]);
+    }
+
+    function getRewardForDuration() external view override returns(uint256) {
+        return rewardRate.mul(rewardsDuration);
+    }
+
+    function budget() public view returns(uint256) {
+        return IERC20(baseToken).balanceOf(address(this));
+    }
+
+    /* ========== MUTATIVE FUNCTIONS ========== */
+
+    function stake(uint256 amount) external override nonReentrant updateReward(msg.sender) {
+        require(amount > 0, "Cannot stake 0");
+        _totalSupply = _totalSupply.add(amount);
+        _balances[msg.sender] = _balances[msg.sender].add(amount);
+        stakingToken.safeTransferFrom(msg.sender, address(this), amount);
+        emit Staked(msg.sender, amount);
+    }
+
+    function withdraw(uint256 amount) public override nonReentrant updateReward(msg.sender) {
+        require(amount > 0, "Cannot withdraw 0");
+        _totalSupply = _totalSupply.sub(amount);
+        _balances[msg.sender] = _balances[msg.sender].sub(amount);
+        stakingToken.safeTransfer(msg.sender, amount);
+        emit Withdrawn(msg.sender, amount);
+    }
+
+    function getReward() public override nonReentrant updateReward(msg.sender) {
+        uint256 reward = rewards[msg.sender];
+        if (reward > 0) {
+            rewards[msg.sender] = 0;
+            rewardsToken.safeTransfer(msg.sender, reward);
+            emit RewardPaid(msg.sender, reward);
+        }
+    }
+
+    function exit() external override {
+        withdraw(_balances[msg.sender]);
+        getReward();
+    }
+
+    /* ========== RESTRICTED FUNCTIONS ========== */
+
+    function buyOnSchedule() external onlyRewardsDistribution {
+        require(rewardsDuration < globalRewardsDuration, "!rewardRatesInvalid");
+
+        buyOnScheduleRate = calculateRate(globalPeriodFinish, globalRewardsDuration, budget(), buyOnScheduleRate);
+        checkRate(baseToken, globalRewardsDuration, buyOnScheduleRate);
+
+        uint256 baseTokensToSpend = chooseMinTime(globalPeriodFinish)
+            .sub(lastBuyOnScheduleTime).mul(buyOnScheduleRate);
+
+        require(baseToken.transferFrom(_msgSender(), address(this), baseTokensToSpend), "!transfer");
+        require(baseToken.approve(address(uniswapRouter), baseTokensToSpend), "!approve");
+
+        address[] memory path = new address[](2);
+        path[0] = address(baseToken);
+        path[1] = address(rewardsToken);
+        uint256 reward = UniswapV2Library.getAmountsOut(uniswapRouter.factory(), baseTokensToSpend, path)[1];
+        uniswapRouter.swapExactTokensForETH(baseTokensToSpend, reward, path, _msgSender(), block.timestamp);
+
+        lastBuyOnScheduleTime = block.timestamp;
+
+        notifyRewardAmount(reward);
+    }
+
+    function calculateRate(uint256 _timeFinish, uint256 _duration, uint256 _rewardAmount, uint256 _oldRate)
+        internal
+        view
+        returns(uint256 rate)
+    {
+      if (block.timestamp >= _timeFinish) {
+          rate = _rewardAmount.div(_duration);
+      } else {
+          uint256 remaining = _timeFinish.sub(block.timestamp);
+          uint256 leftover = remaining.mul(_oldRate);
+          rate = _rewardAmount.add(leftover).div(_duration);
+      }
+    }
+
+    function checkRate(IERC20 _token, uint256 _duration, uint256 _rate) internal {
+        uint256 balance = _token.balanceOf(address(this));
+        require(_rate <= balance.div(_duration), "rateTooHigh");
+    }
+
+    function notifyRewardAmount(uint256 reward)
+        public
+        override
+        onlyRewardsDistribution
+        updateReward(address(0))
+    {
+        rewardRate = calculateRate(periodFinish, rewardsDuration, reward, rewardRate);
+
+        // Ensure the provided reward amount is not more than the balance in the contract.
+        // This keeps the reward rate in the right range, preventing overflows due to
+        // very high values of rewardRate in the earned and rewardsPerToken functions;
+        // Reward + leftover must be less than 2^256 / 10^18 to avoid overflow.
+        checkRate(rewardsToken, rewardsDuration, rewardRate);
+
+        lastUpdateTime = block.timestamp;
+        periodFinish = block.timestamp.add(rewardsDuration);
+
+        emit RewardAdded(reward);
     }
 }
