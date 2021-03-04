349c670d4bd3fe2752f10c3127a6cf3a07686485
IntegralTeam O <repo@sfxdx.ru>
2021-03-04 11:18:20 +0300

    adding test fixtures

---
+++ test/router_test.js
@@ -0,0 +1,22 @@
+/* eslint no-unused-vars: 0 */
+/* eslint eqeqeq: 0 */
+
+const { expect, assert } = require('chai');
+const {
+  BN,
+  constants,
+  expectEvent,
+  expectRevert,
+  ether,
+  time
+} = require('@openzeppelin/test-helpers');
+const { ZERO_ADDRESS } = constants;
+const { ZERO, ONE, getMockTokenPrepared } = require('./utils/common');
+
+const Router = artifacts.require('Router');
+
+const MockContract = artifacts.require("MockContract");
+
+contract('Router', (accounts) => {
+  
+});
