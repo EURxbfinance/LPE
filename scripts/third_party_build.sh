be52bc43ddae640bc2bd8796d44f56bf2e9e86cc
IntegralTeam V <repo@sfxdx.ru>
2021-03-05 14:13:17 +0700

    remove scripts

--- scripts/third_party_build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source ./scripts/utils/generate_truffle_config.sh
-
- # build mock contract
- generate_truffle_config "0.6.6" ".\/node_modules\/@gnosis.pm\/mock-contract\/contracts"
- truffle compile
-
