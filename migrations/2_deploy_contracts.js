072506757459954c0ee462a34f3ecafd94b1d02c
IntegralTeam V <repo@sfxdx.ru>
2021-03-26 16:28:02 +0700

    fix deploy script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -18,23 +18,23 @@ module.exports = function (deployer, network) {
       // do nothing
     } else if (network.startsWith('rinkeby')) {
       const uniswapFactory = await UniswapV2Factory.at('0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f');
-      const eurxbAddress = '0x49Fdb5C0DC55195b5f7AC731e5f5d389925C8c03'; // eurxb rinkeby
+      // const eurxbAddress = '0x49Fdb5C0DC55195b5f7AC731e5f5d389925C8c03'; // eurxb rinkeby
       const xbeAddress = '0xfaC2D38F064A35b5C0636a7eDB4B6Cc13bD8D278'; // XBE rinkeby
       const usdtAddress = '0x909f156198674167a8D42B6453179A26871Fbc96'; // usdt rinkeby
-      const usdtPoolAddress = await uniswapFactory.getPair.call(eurxbAddress, usdtAddress);
+      const usdtPoolAddress = await uniswapFactory.getPair.call(xbeAddress, usdtAddress);
       // deploy router
-      const router = await deployer.deploy(Router);
-      // configure router
-      await router.configure(
-        '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D', // uniswap router
-        eurxbAddress,
-        usdtAddress,
-        process.env.TEAM_RINKEBY_ACCOUNT,
-      );
-      await router.setStartWeights('27000000', '23000000000000000000');
-      const eurxb = ERC20.at(eurxbAddress);
-      const eurxbToRouter = '100000000000000000000000'; // 100 000 eurxb
-      eurxb.transfer(router.address, eurxbToRouter);
+      // const router = await deployer.deploy(Router);
+      // // configure router
+      // await router.configure(
+      //   '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D', // uniswap router
+      //   eurxbAddress,
+      //   usdtAddress,
+      //   process.env.TEAM_RINKEBY_ACCOUNT,
+      // );
+      // await router.setStartWeights('27000000', '23000000000000000000');
+      // const eurxb = await ERC20.at(eurxbAddress);
+      // const eurxbToRouter = '100000000000000000000000'; // 100 000 eurxb
+      // await eurxb.transfer(router.address, eurxbToRouter);
       // deploy incentivizer
       const incentivizer = await deployer.deploy(Incentivizer);
       // configure incentivizer
@@ -44,10 +44,10 @@ module.exports = function (deployer, network) {
         usdtPoolAddress,
         process.env.RINKEBY_REWARDS_DURATION,
       );
-      const xbe = ERC20.at(xbeAddress);
+      const xbe = await ERC20.at(xbeAddress);
       const xbeReward = '100000000000000000000'; // 100 xbe
-      xbe.transfer(incentivizer.address, xbeReward);
-      incentivizer.notifyRewardAmount(xbeReward);
+      await xbe.transfer(incentivizer.address, xbeReward);
+      await incentivizer.notifyRewardAmount(xbeReward);
     } else if (network.startsWith('mainnet')) {
       const uniswapFactory = await UniswapV2Factory.at('0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f');
       const eurxbAddress = '0x0beAD9a1bcc1b84D06E3f2df67E3549Fd55aB054'; // eurxb mainnet
